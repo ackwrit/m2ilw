824ba7524b31ed207381d62468085b348e7ef169
djinodissingar <dissingardjino@icloud.com>
2022-11-18 00:30:29 +0100

    - Mise en place des initialisations

---
+++ android/app/src/main/kotlin/com/example/m2ilw/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.m2ilw
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child:  bodyPage()
+
+      ),
+      backgroundColor: Colors.amber,
+
+
+    );
+
+
+  }
+
+  Widget bodyPage(){
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          //Logo
+          Image.asset("assets/logo.png",width: 100,height: 100,),
+          const SizedBox(height: 10,),
+
+          //Image
+          Image.asset("assets/images.jpg"),
+          const SizedBox(height: 10,),
+
+          //Text d'information
+          const Text("La ligue des justiciers",
+          style: TextStyle(fontSize: 30),
+          ),
+          const SizedBox(height: 10,),
+
+
+          //Bouton cliquable
+          Container(
+            width: double.infinity,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.purple,
+                shape: StadiumBorder()
+              ),
+                onPressed: (){
+                  print("J'ai cliqué");
+                },
+                child: const Text("Bienvenue",
+                style: TextStyle(fontSize: 25),
+                )
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
